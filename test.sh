ebbfdd483a8ea7686b91f561a9cff486ea34da41
glorydome <45866379+glorydome@users.noreply.github.com>
2025-12-27 21:20:48 +0800

    1
    W

--- test.sh
+++ test.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+# Why you not soros.
+# Gods always need positive,if positive,
+# all can do, Prayer just need can do, symbols also.
+# We all need rise vibration, you yes me yes.
+# Everything that if negative purpose are all bullying,
+# only monkeystudio will do bullying.
+# Space growing no edge, Evolution no edge,
+# Vibration higher no edge.Nature Nature till forever ,
+# Rule Rule till forever, Plasma Plasma till forever.
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
